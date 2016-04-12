e47c33079fa46b71b1ab1b4c12ef673e7e33ea70
David Sowerby <david.sowerby@virgin.net>
2016-04-12 10:48:23 +0100

    Fix #43 Refactored and build updated
    Refactored to uk.q3c.build
     Cleaned up gradle.build to remove unused dependencies, and set up publishing

--- src/main/java/uk/q3c/gitplus/gitplus/GitPlusException.java
+++
@@ -1,9 +0,0 @@
-package uk.q3c.gitplus.gitplus;
-
-public class GitPlusException extends RuntimeException {
-
-
-    public GitPlusException(String message, Exception e) {
-        super(message, e);
-    }
-}
